c8f0d7bc4dd691fa4709d55a6b1e3009e197b6e3
ColdMuffin <efrathossain1130@gmail.com>
2025-08-07 12:53:19 -0400

    Added a streamlined setup option and updated to README.md to include it.

--- playwright_testing/sync.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.whatismybrowser.com/detect/what-is-my-user-agent/")
-    page.screenshot(path="playwright_testing/demo.png")
-    browser.close()
