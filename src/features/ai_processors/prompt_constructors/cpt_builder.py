0a33a2df0a1d9248f3a2e8d5ef9c08feb81427d1
ColdMuffin <efrathossain1130@gmail.com>
2025-09-20 16:13:14 -0400

    Annotate CPT Builder file

--- src/features/ai_processors/prompt_constructors/cpt_builder.py
+++ src/features/ai_processors/prompt_constructors/cpt_builder.py
@@ -1,25 +1,31 @@
 
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from typing_extensions import Self
 
 from src.models import SchemaModelFactory
 from src.models import Fields
 
 class ChatPromptTemplateBuilder:
-    def add_parser(self, required_info:str|Fields, factory=None):
+    "Builds a `ChatPromptTemplate` object"
+    def add_parser(self, required_info:str|Fields, factory=None) -> Self:
+        "Adds a `BaseModel` parser to be added to the `ChatPromptTemplate`"
         factory = factory or SchemaModelFactory()
         self.parser = factory.make(required_info)
         return self
 
-    def add_instructions(self, instructions:str):
+    def add_instructions(self, instructions:str) -> Self:
+        "Adds the string system instructions to be added to the `ChatPromptTemplate`"
         self.instructions = instructions
         return self
     
-    def add_query(self, query):
+    def add_query(self, query) -> Self:
+        "Adds a string query to be added to the `ChatPromptTemplate`"
         self.query = query
         return self
 
-    def _create_chat_prompt_template(self):
+    def _create_chat_prompt_template(self) -> None:
+        "Creates a `ChatPromptTemplate` using the components given to itself"
         prompt_obj = ChatPromptTemplate.from_messages(
             [
                 (
@@ -37,7 +43,11 @@ class ChatPromptTemplateBuilder:
                   instructions=self.instructions, 
                   query=self.query)
 
-        return prompt_obj
+        self.chat_prompt_template =  prompt_obj
     
-    def get_chat_prompt_template(self):
-        return self._create_chat_prompt_template()
+    def get_chat_prompt_template(self) -> ChatPromptTemplate:
+        """Returns the `ChatPromptTemplate` object created by the builder
+        or builds it and then returns it if it hasn't been built yet"""
+        if not hasattr(self, "chat_prompt_template"):
+            self._create_chat_prompt_template
+        return self.chat_prompt_template
