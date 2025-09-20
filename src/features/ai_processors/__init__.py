60e8a9262d7807ba097bbee550644c690921766d
ColdMuffin <efrathossain1130@gmail.com>
2025-09-20 17:51:45 -0400

    Add init file

--- src/features/ai_processors/__init__.py
+++ src/features/ai_processors/__init__.py
@@ -1,4 +1,4 @@
 
 from .azure_client import azure_chat_openai, create_chat_prompt_template
 from .prompt_constructors.query_builder import QueryBuilder
-from .prompt_chain import PromptChain
+from .prompt_chain.executor import PromptChainExecutor
