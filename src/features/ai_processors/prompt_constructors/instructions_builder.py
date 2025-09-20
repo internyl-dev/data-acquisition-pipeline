3a170fdaa11d4b2d29366602fe8e72ce948df06d
ColdMuffin <efrathossain1130@gmail.com>
2025-09-20 17:20:41 -0400

    Annotate instructions builder

--- src/features/ai_processors/prompt_constructors/instructions_builder.py
+++ src/features/ai_processors/prompt_constructors/instructions_builder.py
@@ -1,7 +1,10 @@
 
 from .instructions import INSTRUCTIONS
 
+from src.models import Fields
+
 class SystemInstructions:
+    "The system instructions in the prompt to the model"
     def __init__(self):
         self.instructions = ""
 
@@ -9,12 +12,18 @@ class SystemInstructions:
         return self.instructions
 
 class SystemInstructionsBuilder:
+    "Builds the system instructions buy adding to its string representation"
     def __init__(self, instructions_obj=None):
         self.instructions_obj = instructions_obj or SystemInstructions()
 
-    def add_instructions(self, required_info):
-        self.instructions_obj.instructions = INSTRUCTIONS[required_info]
+    def legacy_add_instructions(self, target_info:str):
+        "Creates instructions based on the given string target info"
+        self.instructions_obj.instructions = INSTRUCTIONS[target_info]
         return self
+    
+    def add_instructions(self, target_info:Fields):
+        "Creates instructions based on the given enum target info"
+        self.instructions_obj.instructions = INSTRUCTIONS[target_info.value]
     
     def get_obj(self):
         return self.instructions_obj
