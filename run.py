d0b0692ac610a2d814f79ce002737c2d58ba848d
ColdMuffin <efrathossain1130@gmail.com>
2025-12-29 18:18:34 -0500

    Finish refresher loop

--- run.py
+++ run.py
@@ -11,10 +11,15 @@ db: FirebaseClient = FirebaseClient.get_instance()
 # Input link to extract info from
 links = db.get_all_data("scrape-queue")
 #Pipeline.run(list(links.values())[0]["url"])
-Pipeline.run("https://k12stem.engineering.nyu.edu/programs/cs4cs")
-schema = Pipeline.schema.model_dump()
-pp(schema)
+links = [
+    "https://www.nyhistory.org/education/student-historian-internship-program"
+]
+for link in links:
+    Pipeline.run(link)
+    schema = Pipeline.schema.model_dump()
+    db.save("programs-display", schema, set_index=True)
+    Pipeline.clear()
 
 # Call the function to add it to the 'products' collection
-db.save("programs-display", schema, set_index=True)
+#db.save("programs-display", schema, set_index=True)
 #db.delete_by_id("scrape-queue", list(links.keys())[0])
--- src/utils/refresher.py
+++ src/utils/refresher.py
@@ -1,11 +1,11 @@
 
-from typing import Optional
-from copy import deepcopy
+from typing import Optional, Self
+from datetime import datetime
 from pprint import pp
 
 from src.main import Main
 from src.models import RootSchema
-from src.features.databases import FirebaseClient
+from src.io import FirebaseClient
 
 
 class Refresher:
@@ -14,7 +14,11 @@ class Refresher:
         self.db = FirebaseClient.get_instance()
         self._all_data = {}
 
-    def run(self, link:str, hard:bool=False) -> None:
+    def run(self, link: str, 
+            hard:bool = False, 
+            collection_path: str = "programs-display", 
+            max_days: int = 7, 
+            ignore_condition: bool=False) -> None:
         """
         Get all links from the database
         Rerun with certain preconditions for each
@@ -31,46 +35,72 @@ class Refresher:
         Hard run:\n
         L> All sections are reset
         """
+        if hard:
+            self._hard_run(link, collection_path, max_days, ignore_condition)
+        else:
+            self._soft_run(link, collection_path, max_days, ignore_condition)
         pass
 
     @staticmethod
-    def _refresh_condition(max_days: int) -> bool:
+    def _refresh_condition(schema: RootSchema, max_days: int) -> bool:
         """
         Decides whether or not to refresh a program
         Currently does it based on the date added (if date added is greater than the time specified)
         Args:
+            schema (RootSchema): The schema from which the date added data is taken from
             max_days (int): The amount of days since the date added until an internship can be refreshed
         """
-        return False
+        try:
+            date_added = datetime.strptime(schema.metadata.date_added, "%Y-%m-%d").date()
+        except ValueError:
+            # If date_added is "", a refresh is required anyway
+            return True
+        date_now = datetime.today().date()
+        days_between = (date_now - date_added).days
+        return days_between >= max_days
 
-    def __get_all_data(self, collection_path:str) -> None:
+    def __get_all_data(self, collection_path: str) -> None:
         "Reads the database and gets all data from the specified collection path and sets the non-static variable"
-        self._all_data[collection_path] = self.db.get_all_data(collection_path)
+        # If the database hasn't been read yet, do so
+        if not collection_path in self._all_data:
+            self._all_data[collection_path] = self.db.get_all_data(collection_path)
 
-    def _get_latest_entry(self, collection_path:str, link:str) -> dict[str, dict]:
+    def _get_latest_entry(self, collection_path: str, link: str) -> dict[str, dict]:
         """
         Gets the latest data entry of the specified link
         Returns:
             {id (str): data (dict)}
         """
-        # If the database hasn't been read yet, do so
-        if not collection_path in self._all_data:
-            self.__get_all_data(collection_path)
+        self.__get_all_data(collection_path)
 
         # Documents end with a "-" followed by a number representing the version of the doc
         # Find all data with the same link as the link provided
         docs_with_link: dict[int, dict] = {}
         for doc in self._all_data[collection_path]:
-            id = '-'.join(doc.split('-')[:-1])
-            ver = doc.split('-')[-1]
+            id = "-".join(doc.split("-")[:-1])
+            ver = doc.split("-")[-1]
             if link == id:
                 # {version: {id: data}}
                 docs_with_link[ver] = {id: self._all_data[collection_path][doc]}
 
+        if len(docs_with_link) == 0:
+            raise LookupError(f"No documents with link '{link}' found")
+
         max_ver: int = max(docs_with_link.keys())
         return docs_with_link[max_ver]
 
-    def _soft_run(self, link:str, collection_path: str = "programs-display") -> None:
+    def get_all_latest_entries(self, collection_path: str) -> dict[str, dict]:
+        self.__get_all_data(collection_path)
+
+        all_latest_entries: dict[str, dict] = {}
+        for data in self._all_data[collection_path]:
+            id = "-".join(data.split("-")[:-1])
+            if not id in all_latest_entries:
+                all_latest_entries[id] = self._get_latest_entry(collection_path, id)
+        
+        return all_latest_entries
+
+    def _soft_run(self, link: str, collection_path: str, max_days: int, ignore_condition: bool) -> None:
         doc: RootSchema = RootSchema.model_validate(self._get_latest_entry(collection_path, link)[link])
         cleared_schema = RootSchema()
 
@@ -83,13 +113,29 @@ class Refresher:
         self.pipeline.schema = cleared_schema
         pp(self.pipeline.schema)
 
-        self.pipeline.run(link)
+        if self._refresh_condition(doc, max_days) or ignore_condition:
+            self.pipeline.run(link)
 
-        schema = self.pipeline.schema.model_dump()
-        self.db.save(collection_path, schema, set_index=True)
+            schema = self.pipeline.schema.model_dump()
+            self.db.save(collection_path, schema, set_index=True)
+            print(f"{link} refreshed!")
 
-    def _hard_run(self, link:str) -> None:
-        pass
+    def _hard_run(self, link: str, collection_path: str, max_days: int, ignore_condition: bool) -> None:
+        doc: RootSchema = RootSchema.model_validate(self._get_latest_entry(collection_path, link)[link])
+        cleared_schema = RootSchema()
+        self.pipeline.schema = cleared_schema
+        pp(self.pipeline.schema)
+
+        if self._refresh_condition(doc, max_days) or ignore_condition:
+            self.pipeline.run(link)
+
+            schema = self.pipeline.schema.model_dump()
+            self.db.save(collection_path, schema, set_index=True)
+
+    def clear(self) -> Self:
+        pipeline = self.pipeline.clear()
+        self._all_data = {}
+        return type(self)(pipeline=pipeline)
 
 if __name__ == "__main__":
     r = Refresher()
@@ -98,4 +144,7 @@ if __name__ == "__main__":
     print(r._get_latest_entry("programs-display", "https:\\\\opportunitynetwork.org\\apply\\"))
     print(r._get_latest_entry("programs-display", "https:\\\\sumac.spcs.stanford.edu\\sumac-online-program"))
     """
-    r._soft_run("https:\\\\aimi.stanford.edu\\education\\summer-research-internship")
+    links = list(r.get_all_latest_entries("programs-display").keys())[0:10]
+    for link in links:
+        r.run(link)
+        r.clear()
